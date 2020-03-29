c725a613d13df4ac4a59423ea8b803381617d482
saurav-555 <57410861+saurav-555@users.noreply.github.com>
2020-03-29 14:20:01 +0530

    Process Id Manager(2) : 2 more Thread added
    Fibonacci Thread is used to generate Fibonacci numbers.
    Factorial Thread is used to generate factorial numbers.

--- main.cpp
+++ main.cpp
@@ -27,11 +27,65 @@ void *timeThread(void *arg){
 
 }
 
+void *factorialThread(void * arg){
 
+    int threadId = *((int *)arg);
+
+    // allocating pid
+
+    int upto =  rand() % 10 + 1;
+    printf("Thread of Factorial( Random upto %d ) | Thread Id : %d | Pid : %d \n" , upto,  threadId , 0);
+    printf("Factorial upto %d : ", upto);
+
+    int fac = 1;
+    for(int i = 1 ; i <= upto ; i++){
+        fac = fac * i;
+        printf("%d " , fac);
+    }
+    printf("\n\n");
+
+    // wait random period of time bw 3 to 10 sec
+
+    sleep(3 + rand() % 8);
+
+    return NULL;
+
+}
+
+void *fibonacciThread(void * arg){
+
+    int threadId = *((int *)arg);
+
+    // allocating pid
+
+    int upto =  rand() % 10 + 2;
+    printf("Thread of Fibonacci( Random upto %d ) | Thread Id : %d | Pid : %d \n" , upto,  threadId , 0);
+    printf("Fibonacci upto %d : ", upto);
+
+    int f0 = 0 , f1 = 1;
+    printf("f0 = %d | f1 = %d | " , f0 , f1);
+    int f2;
+    for(int i = 2 ; i <= upto ; i++){
+        f2 = f0 + f1;
+        printf("f%d = %d | " , i , f2);
+        f0 = f1;
+        f1 = f2;
+    }
+    printf("\n\n");
+
+    // wait random period of time bw 3 to 10 sec
+
+    sleep(3 + rand() % 8);
+
+    return NULL;
+
+}
 
 void solve() {
     int i = 5;
     timeThread((void *)(&i));
+    factorialThread((void *)(&i));
+    fibonacciThread((void *)(&i));
 }
 
 
@@ -43,4 +97,5 @@ int main() {
     while (t--)
         solve();
     return 0;
+
 }
